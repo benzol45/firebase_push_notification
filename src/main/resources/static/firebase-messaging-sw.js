667443e8993f4829e6c7ed8161db469a45056e8b
Михаил Калинин <benzol.45@gmail.com>
2023-08-04 17:29:52 +0200

    new account

--- src/main/resources/static/firebase-messaging-sw.js
+++ src/main/resources/static/firebase-messaging-sw.js
@@ -3,7 +3,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
-    messagingSenderId: '668901969584'
+    messagingSenderId: '1095413981860'
 });
 
 const messaging = firebase.messaging();
